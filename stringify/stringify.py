f91596c23a86417fcc10238ed5076d3d32d520b2
Alexander Shangin <alarriva@yandex.ru>
2023-04-28 07:04:47 +0600

    Fix files

--- stringify/stringify.py
+++ stringify/stringify.py
@@ -9,15 +9,35 @@ def stringify(value, replacer=' ', spaces_count=1):
         indent = replacer * spaces_count
         result = '{\n'
         for k, v in value.items():
-            result += f'{indent}{k}: {stringify(v, replacer, spaces_count+2)}\n'
-        result += f'{replacer * (spaces_count-2)}}}'
+            result += f'{indent}{k}: {stringify(v, replacer, spaces_count+1)}\n'
+# result += f'{indent}{k}: {replacer}{stringify(v, replacer, spaces_count+2)}\n'
+        result += f'{replacer * (spaces_count-1)}}}'
         return result
     else:
         raise ValueError(f'Unsupported value type: {type(value)}')
-    
+ 
 
-data = { "hello": "world", "is": True, "nested": { "count": 5 } }
-   
+data = {
+    "string": "value",
+    "boolean": True,
+    "number": 5,
+    "dict": {
+        5: "number",
+        None: "None",
+        True: "boolean",
+        "value": "string",
+        "nested": {
+            "boolean": True,
+            "string": 'value',
+            "number": 5,
+            None: "None",
+        },
+    },
+}
+
+# data = { "hello": "world", "is": True, "nested": { "count": 5 } }
+# data = True   
+
 # print(stringify(data, replacer=' ', spaces_count=1))
 
-print(stringify(data, 'Z', 2))
+print(stringify(data, '|-', 2))
--- tests/test_stringify.py
+++ tests/test_stringify.py
@@ -4,23 +4,25 @@ import pytest
 
 from stringify.stringify import stringify
 
-
+# Вернуть полный путь к файлу в директории fixtures
 def get_fixture_path(file_name):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(current_dir, 'fixtures', file_name)
 
+# Прочитать файл по указанному пути
 def read(file_path):
     with open(file_path, 'r') as f:
         result = f.read()
     return result
 
-
+# Создаем несколько тестовых значений примитивных типов данных для проверки функции stringify()
 primitives = {
     "string": "value",
     "boolean": True,
     "number": 5,
 }
 
+# Создаем сложную вложенную структуру данных для проверки функции stringify()
 nested = {
     "string": "value",
     "boolean": True,
@@ -39,29 +41,35 @@ nested = {
     },
 }
 
+# Создаем список тестовых случаев для проверки функции stringify()
+# Каждый случай содержит три параметра: заменитель (replacer), количество пробелов (spases_count) и индекс 
+# соответствующей строки в файлах plain.txt и nested.txt
 cases = [
-    ('|-', 1, 0),
-    ('|-', 2, 1),
-    (' ', 3, 2),
+    ('|-', 1, 0),  # первый уровень вложенности, 1 пробел
+    ('|-', 2, 1),  # второй уровень вложенности, 2 пробела
+    (' ', 3, 2),   # третий уровень вложенности, 3 пробела
 ]
 
-
+# Тест функции stringify() на примитивных типах данных
+# Проверяем, что функция правильно преобразует строку, логическое значение и число в строку
 @pytest.mark.parametrize("value", primitives.values())
 def test_primitives(value):
     assert stringify(value) == str(value)
 
-
-# expected_data = {"nested": [], "plain": []}
 plain_data = read(get_fixture_path('plain.txt')).rstrip().split('\n\n\n')
 nested_data = read(get_fixture_path('nested.txt')).rstrip().split('\n\n\n')
 
-
+# Тест функции stringify() на сложной вложенной структуре данных
+# Проверяем, что функция правильно преобразует словарь с вложенными словарями и списками в строку с заданным 
+# количеством пробелов и заменителями
 @pytest.mark.parametrize("replacer,spases_count,case_index", cases)
 def test_plain(replacer, spases_count, case_index):
     expected = nested_data[case_index]
     assert stringify(nested, replacer, spases_count) == expected
 
-
+# Тест функции stringify() на примитивных типах данных
+# Проверяем, что функция правильно преобразует словарь с примитивными типами данных в строку с заданным 
+# количеством пробелов и заменителями
 @pytest.mark.parametrize("replacer,spases_count,case_index", cases)
 def test_nested(replacer, spases_count, case_index):
     expected = plain_data[case_index]
